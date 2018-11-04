e5c0a53c670f78f350a8e9e8b9982d20e7ea6502
hpoggie <henry.poggie@gmail.com>
2018-11-04 07:42:48 -0500

    Add .bam to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
             'include_patterns': [
                 '*.png',
                 '*.ttf',
+                '*.bam',
                 'CREDITS.txt'
             ],
             'plugins': [
