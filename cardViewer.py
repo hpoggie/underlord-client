ff35f9167525d4034518eff347eab8950e130315
hpoggie <henry.poggie@gmail.com>
2018-12-19 22:00:25 -0500

    Let you choose different cards with the viewer

--- cardViewer.py
+++ cardViewer.py
@@ -9,15 +9,13 @@ import cardBuilder
 
 panda3d.core.loadPrcFileData('', 'model-path assets')
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('-f', type=str, default='ul_core.factions.templars')
-#parser.add_argument('-c', type=str, default='Equus')
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', type=str, default='templars')
+parser.add_argument('-c', type=str, default='equus')
+args = parser.parse_args()
 
-#mod = importlib.import_module(args.f)
-#card = getattr(mod, args.c)()
-
-import ul_core.factions.templars as templars
+mod = importlib.import_module('ul_core.factions.' + args.f)
+card = getattr(mod, args.c)()
 
 base = direct.showbase.ShowBase.ShowBase()
 base.disableMouse()
@@ -26,7 +24,6 @@ class FakePlayer:
     iconPath = 'templar_icons'  # Hack to make icon paths work
     manaCap = 0  # Equus rank hack
 
-card = templars.equus()
 card.owner = FakePlayer()
 node = base.render.attachNewNode('card_root')
 node.setPos(0, 5, 0)
