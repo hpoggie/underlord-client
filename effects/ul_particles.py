f6a18d7d24ac05f83de990778179e566944c3b80
Henry Poggie <henry.poggie@gmail.com>
2019-02-22 19:45:10 -0500

    Use nicer path for effect

--- effects/ul_particles.py
+++ effects/ul_particles.py
@@ -1,7 +1,5 @@
 import particles.vfx_loader
 from panda3d.core import loadPrcFileData
 
-loadPrcFileData('', 'model-path assets/effects/dust')
-
 def load_dust():
-    return particles.vfx_loader.load('assets/effects/dust/dust.json')
+    return particles.vfx_loader.load('effects/dust/dust.json')
