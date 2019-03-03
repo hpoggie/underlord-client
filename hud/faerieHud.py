6ba04fd8809f202ab78ca32be31501c2008835d7
Henry Poggie <henry.poggie@gmail.com>
2019-03-02 19:47:24 -0500

    Faerie hud allows you to hold cards

--- hud/faerieHud.py
+++ hud/faerieHud.py
@@ -2,14 +2,14 @@ import hud.game
 
 
 class FaerieHud(hud.game.GameHud):
-    def onEndPhaseButton(self):
+    def onEndTurnButton(self):
         def callback(card):
-            base.endPhase(card)
+            base.endTurn(card)
             base.finishTargeting()
 
         if len(base.player.facedowns) == 0:
-            super().onEndPhaseButton()
+            super().onEndTurnButton()
         elif len(base.player.facedowns) == 1:
-            base.clientActions.endPhase([base.player.facedowns[0]])
+            base.clientActions.endTurn([base.player.facedowns[0]])
         else:
             base.mouseHandler.startTargeting("Choose card to keep", callback)
