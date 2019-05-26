4365524048dd3c1301626eed08fe88ff8e012027
Henry Poggie <henry.poggie@gmail.com>
2019-05-26 14:56:31 -0400

    Queue updates

--- protocol/rpcReceiver.py
+++ protocol/rpcReceiver.py
@@ -2,6 +2,13 @@ import queue
 import ul_core.factions
 
 
+def queued_update(func):
+    def do_update(self, *args, **kwargs):
+        self.update_queue.put(lambda: func(self, *args, **kwargs))
+
+    return do_update
+
+
 class RpcReceiver:
     """
     Translates server messages into game state changes
@@ -138,56 +145,73 @@ class RpcReceiver:
         for pl in self.state.game.players:
             pl.hasMulliganed = True
 
+    @queued_update
     def updatePlayerHand(self, *cards):
         self.updateZone(self.state.player.hand, cards)
 
+    @queued_update
     def updateEnemyHand(self, *cards):
         self.updateZone(self.state.enemy.hand, cards)
 
+    @queued_update
     def updatePlayerFacedowns(self, *cards):
         self.updateZone(self.state.player.facedowns, cards)
 
+    @queued_update
     def updateEnemyFacedowns(self, *cards):
         self.updateZone(self.state.enemy.facedowns, cards)
 
+    @queued_update
     def updatePlayerFaceups(self, *cards):
         self.updateZone(self.state.player.faceups, cards)
 
+    @queued_update
     def updateHasAttacked(self, *values):
         for i, c in enumerate(self.state.player.faceups):
             c.hasAttacked = values[i]
 
+    @queued_update
     def updateEnemyFaceups(self, *cards):
         self.updateZone(self.state.enemy.faceups, cards)
 
+    @queued_update
     def updatePlayerGraveyard(self, *cards):
         self.updateZone(self.state.player.graveyard, cards)
 
+    @queued_update
     def updateEnemyGraveyard(self, *cards):
         self.updateZone(self.state.enemy.graveyard, cards)
 
+    @queued_update
     def updatePlayerManaCap(self, manaCap):
         self.state.player.manaCap = manaCap
 
+    @queued_update
     def updatePlayerMana(self, mana):
         self.state.player.mana = mana
 
+    @queued_update
     def updateEnemyManaCap(self, manaCap):
         self.state.enemy.manaCap = manaCap
 
+    @queued_update
     def updatePlayerCounter(self, index, value):
         self.state.player.faceups[index].counter = value
 
+    @queued_update
     def updateEnemyCounter(self, index, value):
         self.state.enemy.faceups[index].counter = value
 
+    @queued_update
     def updatePlayerFacedownStaleness(self, *values):
         for i, c in enumerate(self.state.player.facedowns):
             c.stale = values[i]
 
+    @queued_update
     def updateEnemyFacedownStaleness(self, *values):
         for i, c in enumerate(self.state.enemy.facedowns):
             c.stale = values[i]
 
+    @queued_update
     def setActive(self, value):
         self.state.active = value
