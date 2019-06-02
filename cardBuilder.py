25005ad0d2ce2a7847b4261160628caa9ae75b4b
Henry Poggie <henry.poggie@gmail.com>
2019-06-01 22:31:47 -0400

    Rotate on hidden cards

--- cardBuilder.py
+++ cardBuilder.py
@@ -90,6 +90,7 @@ def buildCard(card, parent):
 def buildBlankCard(card, parent):
     cardBase = parent.attachNewNode('mysterious card')
     cardFrame = makeCardFrame(cardBase)
+    cardFrame.setHpr(0, -90, 0)
     cardBase.setPythonTag('card', card)
     card.pandaNode = cardBase
     cardBase.setCollideMask(cardCollisionMask)
